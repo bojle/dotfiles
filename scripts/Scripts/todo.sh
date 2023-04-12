a3372dff08b450fe2fcc293144b8da0d04701be9
Shreeyash Pandey <shreeyash335@gmail.com>
2023-04-12 23:02:06 +0530

    todo: add checking for connection before fetching and refactor
    not checking for connection to the internet was disastorous in that, the
    entire issue would be over-written to be blank as no file was fetched to
    be written back. This fixes it.
    
    operations are refactored into sensible functions
    
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- scripts/Scripts/todo.sh
+++ scripts/Scripts/todo.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+# Depends on gh (github.com/cli/cli)
+
 todo_list_path=$HOME/dev/todo-list
-current_dir="$(pwd)"
-issue_no=1
 todo_file="todo.md"
-cd $todo_list_path
-if [[ $issue_no = 1 ]]; then
+current_dir="$(pwd)"
+
+function check_connection() {
+	ret="$(curl -Is 'https://www.google.com' | head -n 1)"
+	echo "Checking Connection..."
+	if [[ "$ret" == "HTTP/2 200" ]]; then
+		echo "Error: Couldn't Connect to github.com"
+		exit 1
+	fi
+}
+
+function fetch_file() {
 	echo "Fetching..."
 	gh issue view "https://github.com/bojle/todo-list/issues/1#issue-1604396469" > /tmp/todo_tmp_
-fi
-sed 's/\r//g' /tmp/todo_tmp_ > $todo_file
-vim $todo_file
-echo "Writing..."
-gh issue edit "$issue_no" -F $todo_file
+}
+
+function open_file() {
+	sed 's/\r//g' /tmp/todo_tmp_ > $todo_file
+	vim $todo_file
+}
+
+function write_back() {
+	echo "Writing..."
+	gh issue edit 1 -F $todo_file
+}
+
+
+cd $todo_list_path
+check_connection
+fetch_file
+open_file
+write_back
 cd "$current_dir"
