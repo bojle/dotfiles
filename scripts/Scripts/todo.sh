191b71528165f91695c9d8a78dc9dfc6df3bb3bb
Shreeyash Pandey <shreeyash335@gmail.com>
2023-05-17 13:07:26 +0530

    todo: exit without write_back if vim fails
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- scripts/Scripts/todo.sh
+++ scripts/Scripts/todo.sh
@@ -7,7 +7,7 @@ todo_file="todo.md"
 current_dir="$(pwd)"
 
 function check_connection() {
-	echo "Checking Connection..."
+	echo "Checking Connection"
 	ret="$(curl -Is 'https://www.google.com' | head -n 1)"
 	if [ -z "$ret" ]; then
 		echo "Error: Couldn't Connect to github.com"
@@ -16,17 +16,22 @@ function check_connection() {
 }
 
 function fetch_file() {
-	echo "Fetching..."
+	echo "Fetching"
 	gh issue view "https://github.com/bojle/todo-list/issues/1#issue-1604396469" > /tmp/todo_tmp_
 }
 
 function open_file() {
 	sed 's/\r//g' /tmp/todo_tmp_ > $todo_file
 	vim $todo_file
+	# quit if vim err'ed
+	if (( $? == 1 )); then
+		echo "Exit without write_back"
+		exit 1
+	fi
 }
 
 function write_back() {
-	echo "Writing..."
+	echo "Writing"
 	gh issue edit 1 -F $todo_file
 }
 
